fe237ef8828cc912697bc71b48b9d13d481b9295
julie.chiang <julie.chiang@langlive.com>
2020-04-13 21:02:27 +0800

    adjust configs

--- configs/index.js
+++ configs/index.js
@@ -9,11 +9,13 @@ if (config.env === "development") {
   // local
   require("dotenv").config();
   overrides = require("./config.json");
-  overrides.address.rss = process.env.RSS_ADDRESS || overrides.address.rss;
-  overrides.address.notification.webhook = process.env.NOTI_WEBHOOK_URL;
 } else {
   overrides = require(`./config_${config.env}.json`);
 }
+
+overrides.address.rss = process.env.RSS_ADDRESS || overrides.address.rss;
+overrides.address.notification.webhook = process.env.NOTI_WEBHOOK_URL;
+
 Object.assign(config, overrides);
 
 module.exports = config;
